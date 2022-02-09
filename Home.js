247145b9f6c9fe4f929c56b8baabe8b1e13e2803
Hadar-Cohen <hadar5z5z5@gmail.com>
2022-02-09 13:54:36 +0200

    hadar

--- Home.js
+++ Home.js
@@ -7,7 +7,7 @@ export default function Home({navigation}) {
   return (
     <View>
       <Text>
-          _Timely_
+          Timely Hadar!!!
       </Text>
       <Button title='Look For a ride'  onPress={()=> navigation.navigate('Map')}/>
 
