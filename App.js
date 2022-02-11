bbd5d916e68025bdcf53a8de7c53d744ac721c47
dinb95 <83233503+dinb95@users.noreply.github.com>
2022-02-11 16:47:18 +0200

    login changes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import React, {useState} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
@@ -7,8 +8,16 @@ import NavigationComp from './Components/NavigationComp';
 
 
 export default function App() {
+  const [logged, setLogged] = useState(false);
+  const logUser = (user) =>{
+    console.log(user)
+    setLogged(true);
+  }
   const Drawer = createDrawerNavigator();
-  return (
+  if(!logged){
+    return <LoginScreen setLogged={logUser}/>
+  }
+  else return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Main">
         <Drawer.Screen name="Search Menu" component={NavigationComp} />
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, View, Button } from "react-native";
 import * as Google from "expo-google-app-auth";
 
-const LoginScreen = ({ navigation }) => {
+const LoginScreen = ({setLogged}) => {
   const signInAsync = async () => {
     console.log("LoginScreen.js 6 | loggin in");
     try {
@@ -14,7 +14,8 @@ const LoginScreen = ({ navigation }) => {
       if (type === "success") {
         // Then you can use the Google REST API
         console.log("LoginScreen.js 17 | success, navigating to profile");
-        navigation.navigate("Profile", { user });
+        setLogged(user)
+        //navigation.navigate("Profile", { user });
       }
     } catch (error) {
       console.log("LoginScreen.js 19 | error with login", error);
@@ -30,4 +31,11 @@ const LoginScreen = ({ navigation }) => {
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    height:'100%',
+    width: '100%'
+  }
+});
