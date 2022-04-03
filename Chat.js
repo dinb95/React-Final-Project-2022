71a365731084880de435a81f7f85e5d63d4f02b1
Hadar-Cohen <hadar5z5z5@gmail.com>
2022-04-03 13:19:12 +0300

    hc

---
+++ Chat.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Fire from './Fire.js'
+
+
+class Chat extends React.Component {
+    
+    state = {
+        messages: [],
+      };
+    componentDidMount() {
+        Fire.shared.on(message =>
+          this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, message),
+          }))
+        );
+    }
+    componentWillUnmount() {
+        Fire.shared.off();
+      }
+    get user() {
+        return {
+          name: Hadar, //await AsyncStorage.getItem('username'),
+          _id: Fire.shared.uid,
+        };
+      }
+
+  render() {
+    return (
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={Fire.shared.send}
+          user={this.user}
+        />
+      );
+  }
+}
+const styles = StyleSheet.create({});
+export default Chat;
---
+++ Components/NavigationComp.js
@@ -0,0 +1,31 @@
+import { View, Text } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react'
+import Home from './Home';
+import Map from './Map';
+import SearchRoute from './SearchRoute';
+import ProfileScreen from '../screens/ProfileScreen';
+import Prediction from './Prediction';
+import AlarmClock from '../screens/AlarmClock';
+import UserLocation from './UserLocation';
+
+import ChatScreen from '../screens/ChatScreen';
+import Chat from './Chat';
+
+export default function NavigationComp() {
+    const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Search Route" component={SearchRoute}/>
+      <Stack.Screen name="Prediction Process" component={Prediction}/>
+      <Stack.Screen name="Map" component={Map} options={{headerShown:true}}/>
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="AlarmClock" component={AlarmClock} />
+      <Stack.Screen name="UserLocation" component={UserLocation} />
+      <Stack.Screen name="ChatScreen" component={ChatScreen} />
+      <Stack.Screen name="Chat" component={Chat} />
+
+    </Stack.Navigator>
+  )
+}
